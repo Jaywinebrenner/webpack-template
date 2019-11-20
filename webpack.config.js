e5e5666714fb5a88780e77c5a0a3aeb8f2eb11f5
Jay Winebrenner <jaywinebrenner@gmail.com>
2019-11-19 21:06:00 -0800

    template time

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const Dotenv = require('dotenv-webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require ('clean-webpack-plugin');
+const UglifyJsPlugin = require ('uglifyjs-webpack-plugin');
+
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devtool: 'eval-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
+  plugins: [
+    new UglifyJsPlugin({ sourceMap: true }),
+    new Dotenv(),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin ({
+      title: 'Sudoku',
+      template: './src/index.html',
+      inject: 'body'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "eslint-loader"
+      }
+    ]
+  }
+};
